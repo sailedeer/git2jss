0dc0cd580d34d1ae50099e0375b14c93390279bd
Eli Reed <erobreed@uw.edu>
2020-05-04 14:01:14 -0700

    Added some exceptions for repo class

--- lib/git2jss/exceptions.rb
+++ lib/git2jss/exceptions.rb
@@ -1,2 +1,6 @@
-class ParameterError < RuntimeError; end
-
+module Git2JSS
+  class ParameterError < RuntimeError; end
+  class NotAGitRepoError < RuntimeError; end
+  class NoRemoteError < RuntimeError; end
+  class TooManyRemotesError < RuntimeError; end
+end
