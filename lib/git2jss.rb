c61520504f14c2ee36b22f719e9f703f56de9579
Eli Reed <erobreed@uw.edu>
2020-05-19 15:20:10 -0700

    Moved actual code here

--- lib/git2jss.rb
+++ lib/git2jss.rb
@@ -1,7 +1,32 @@
 # load the dependencies
 require 'ruby-jss'
-require 'keyring'
 
-# load the module
+# load the rest of the module
 require 'git2jss/prefs'
 require 'git2jss/repo'
+require 'git2jss/parser'
+
+module Git2JSS
+  class Git2JSS
+    # acts as main method for executable
+    def self.run(args)
+      options = get_args args
+      if good_args? options
+      
+    end
+
+    private
+
+    def get_args(args)
+      parser = Git2JSS::Parser.new
+      parser.parse
+      parser
+    end
+
+    def good_args?(options)
+      
+    end
+
+  end   # class Git2JSS
+end   # module Git2JSS
+
