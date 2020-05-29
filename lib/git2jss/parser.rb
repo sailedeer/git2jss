08c9ef2d92199b879c3ba7bb2068067c9271da7e
Eli Reed <erobreed@uw.edu>
2020-05-29 13:51:56 -0700

    Methods for each option now specified

--- lib/git2jss/parser.rb
+++ lib/git2jss/parser.rb
@@ -4,22 +4,24 @@ require 'git2jss/version'
 module Git2JSS
   class Parser
     attr_reader :files, :all, :names, :source_dir, :branch, :tag, :prefs,
-                :verbose
+                :verbose, :dry, :info
   
     def initialize
-      self.files = []
-      self.all = false
-      self.names = []
-      self.source_dir = "."
-      self.branch = nil
-      self.tag = nil
-      self.prefs = nil
-      self.verbose = false
+      @files = []
+      @all = false
+      @names = []
+      @source_dir = "."
+      @branch = nil
+      @tag = nil
+      @prefs = nil
+      @info = []
+      @verbose = false
+      @dry = false
     end
 
-    def parse
-      options = OptionParser.new do |p|
-        define_options p 
+    def parse!
+      options = OptionParser.new do |opts|
+        define_options opts
       end
       options.parse!
     end
@@ -40,66 +42,73 @@ module Git2JSS
       tag_option parser
       info_option parser
       verbose_option parser
+      dry_run_option parser
   
       parser.separator ""
       parser.separator "Common options:"
   
-      parser.on_tail "-h", "--help", "Show this help message"  do
+      parser.on_tail "-h", "--help", "Show this help message" do
         puts parser
         exit
       end
   
-      parser.on_tail "--version", "Show version"  do
+      parser.on_tail "--version", "Show version" do
         puts Git2JSS::VERSION
         exit
       end
     end
   
     def all_option(parser)
-      parser.on "-a", "--all", "Copy all files to JSS"  do
-        self.all = true
+      parser.on "-a", "--all", "Copy all files to JSS" do
+        @all = true
       end
     end   
   
     def files_option(parser)
-      parser.on "-f", "--files f1,f2,f3", Array, "List of files"  do |f|
-        self.files = f
+      parser.on "-f", "--files f1,f2,f3", Array, "List of files" do |f|
+        @files = f
       end
     end
   
     def names_option(parser)
-      parser.on "-n", "--names n1,n2,n3", Array, "List of file names"  do |n|
-        self.names = n
+      parser.on "-n", "--names n1,n2,n3", Array, "List of file names" do |n|
+        @names = n
       end
     end
   
     def source_dir_option(parser)
-      parser.on "-s", "--source-dir [DIRECTORY]", "Location of repo"  do |s|
-        self.source_dir = s
+      parser.on "-s", "--source-dir DIRECTORY", String, "Path to repo" do |s|
+        @source_dir = s
       end
     end
   
     def branch_option(parser)
-      parser.on "-b", "--branch BRANCH", "Target branch"  do |b|
-        self.branch = b
+      parser.on "-b", "--branch BRANCH", "Target branch" do |b|
+        @branch = b
       end
     end
   
     def tag_option(parser)
-      parser.on "-t", "--tag TAG", "Target tag"  do |t|
-        self.tag = t
+      parser.on "-t", "--tag TAG", "Target tag" do |t|
+        @tag = t
       end
     end
   
     def info_option(parser)
-      parser.on "-i", "--info [PREF FILE]", "Print JSS configuration"  do
-        # check if file exists/build the object?
+      parser.on "-i", "--info [PREF FILE]", "Print JSS configuration" do
+        
       end
     end
   
     def verbose_option(parser)
-      parser.on "-v", "--version", "Run verbosely"  do
+      parser.on "-v", "--version", "Run verbosely" do
         self.verbose = true
+      end
+    end
+
+    def dry_run_option(parser)
+      parser.on "-d", "--dry-run", "Do everything but make changes to the JSS" do
+        self.dry = true
       end
     end
   end   # class CLIParser
