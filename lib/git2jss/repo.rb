67e80f45b97cdaa21ee41f74f966256cbf06d26a
Eli Reed <erobreed@uw.edu>
2021-12-20 15:02:37 -0800

    formatting and remove references to subprocess

--- lib/git2jss/repo.rb
+++ lib/git2jss/repo.rb
@@ -1,5 +1,4 @@
 require 'tmpdir'
-require 'subprocess'
 require 'git'
 require 'fileutils'
 
@@ -26,35 +25,29 @@ module Git2JSS
       # attempt to capture the name of the remote
       begin
         @remote_name = get_remote_name
-      rescue Subprocess::NonZeroExit => e
-        if e.include? "not a git repository"
-          raise NotAGitRepoError, "Not a git repo"
-        else
-          raise RuntimeError, "Unknown error"
-        end
       rescue RuntimeError => e
         # this is extremely bad and I need to check for each error explicitly
         puts e
-      end
+      end # begin
 
       @temp_repo_dir = Dir.mktmpdir
       @remote_uri = get_remote_uri
 
       # attempt to clone the remote
       @temp_repo_dir = clone_to_temp_dir
-    end
+    end # initialize
 
     # does the file exist at the ref?
     def has_file?(name)
       name = File.join(@temp_repo_dir, name)
       File.exist? name
-    end
+    end # has_file?
 
     # retrieve a File object which corresponds to the specified file
     def get_file(name)
       name = File.join(@temp_repo_dir, name)
       File.new name, "r"
-    end
+    end # get_file?
 
     private
 
@@ -65,9 +58,9 @@ module Git2JSS
       rescue Git::GitExecuteError
         FileUtils.remove_dir("#{@temp_repo_dir}", force: true)
         temp_repo = Git.clone("#{@remote_uri}", "#{@temp_repo_dir}")     
-      end
+      end # begin
       return temp_repo.dir.to_s
-    end
+    end # clone_to_temp_dir
 
     # retrieves the name of the remote (usually origin)
     def get_remote_name
@@ -79,14 +72,14 @@ module Git2JSS
         raise NoRemoteError, "No Git remote is configured."
       else
         remotes[0].name
-      end
+      end # get_remote_name
     end
 
     # retrieves the URI of the remote
     def get_remote_uri
       remotes = @git.remotes
       return remotes[0].url
-    end
+    end # get_remote_uri
   end
 end
 
