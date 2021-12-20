6291e0eaeb188944fc10ba542cd946b642e6769b
Eli Reed <erobreed@uw.edu>
2021-12-20 15:00:19 -0800

    made some changes to command line argument checking

--- lib/git2jss.rb
+++ lib/git2jss.rb
@@ -13,12 +13,14 @@ module Git2JSS
     # acts as main method for executable
     def self.run(args)
       # parse the args
-      options = self.get_args args
+      @ignore_names = false
+      options = self.get_args(args)
 
       # verify that args are reasonable
-      if not self.good_args? options
+      if not self.good_args?(options)
+        puts(options.help)
         exit(1)
-      end
+      end # if
 
       # get to work
       ref = (options.tag or options.branch)
@@ -26,36 +28,37 @@ module Git2JSS
       push_map = {}
       repo = nil
 
-      # this raises a few errors, will need to check for each one
       begin
         repo = GitRepo.new ref, options.source_dir
-      rescue ArgumentError
-        puts "Unable to build GitRepo object."\
-              "Please ensure that the source directory is actually a git repository."
+      rescue Error
+        puts("Unable to build GitRepo object. "\
+              "Please ensure that the source directory "\
+              "is actually a git repository."
         exit(1)
-      rescue Error => e
-        puts "Unable to build GitRepo object."\
-        "Please ensure that the source directory is actually a git repository."
-        exit(1)
-      end
+      end # begin
 
+      # TODO: update name map to use file names as keys if
+      # there isn't an object with that name already in the JSS
+      # TODO: handle ignore names case, wherein we'll use the file name
+      # as the key for all files.
+      # TODO: handle all files case (basically the above).
       # build a list of valid files
       (0...options.files.length).each do |i|
         if repo.has_file? options.files[i]
           push_map[name_sym_list[i]] = options.files[i]
         else
-          puts "#{options.files[i]} not found at given ref"
-        end
-      end
+          puts("WARNING: \"#{options.files[i]}\" does not exist on ref \"#{ref}\".")
+        end # if
+      end # each loop
 
-      prefs = KeyringJSSPrefs.new(keyring: true, file: true)
-      # 0. connect to the JSS
+      prefs = KeyringJSSPrefs.new(keyring: options.use_keyring, file: options.file)
       begin
         JSS.api.connect(user: "#{prefs.user}", pw: "#{prefs.pw}", server: "#{prefs.fqdn}")
       rescue JSS::AuthenticationError
-        puts "Failed to authenticate against the JSS. Check that your password is correct."
-        # TODO: prompt for a password and update the keychain
-      end
+        puts("Failed to authenticate against the JSS. Check that your password is correct.")
+        exit(1)
+      end # begin
+
       script_names_in_jss = JSS::Script.all_names
       push_map.each do |k, v|
         code = File.read(File.join(options.source_dir, v))
@@ -65,20 +68,19 @@ module Git2JSS
           # script objects exists in JSS already - let's update
           script = JSS::Script.fetch(name: k.to_s)
           t = Time.new
-          # TODO: add the commit that corresponds with this file - how to get this?
           if not script.notes
             script.notes = ""
-          end
+          end # if
           if script.notes[-1] != "\n"
             script.notes += "\n"
-          end
+          end # if
           script.notes += "Script updated on #{t.inspect} from ref #{ref}\n"
         else
           # script object doesn't exist in JSS already - let's create
           script = JSS::Script.make(name: k.to_s)
           t = Time.new
           script.notes = "Script object created on #{t.inspect} from ref #{ref}\n"
-        end
+        end # if
         script.code = code
         if options.dry
           # print the results, but don't save them to the JSS
@@ -87,9 +89,9 @@ module Git2JSS
           puts script.info
         else
           script.save
-        end
-      end
-    end
+        end # if
+      end # each block
+    end # self.run
 
     private
 
@@ -97,30 +99,28 @@ module Git2JSS
       parser = Parser.new
       parser.parse!
       parser
-    end
+    end # self.get_args
 
     def self.good_args?(options)
-      if options.tag and not options.branch
-        return true
-      elsif options.branch and not options.tag
-        return true
-      elsif not options.branch and not options.tag and options.info_flag
-        # attempt to initialize a prefs object with the info
-        begin
-          args = {:file => options.info}
-          prefs = KeyringJSSPrefs.new args
-          puts "FQDN: #{prefs.fqdn}"
-          puts "USER: #{prefs.user}"
-        rescue ParameterError => pe
-          puts pe.message
-        ensure
-          return false
-        end
-      else
-        puts options.help
-        return false
-      end
-    end
-  end   # class Git2JSS
-end   # module Git2JSS
+      if options.all and options.names
+        puts("WARNING: --all was specified, ignoring --names.")
+        @ignore_names = true
+      end # if
 
+      if not options.names
+        puts("WARNING: --names was not specified. Ignoring.")
+        @ignore_names = true
+      end # if
+
+      if not options.info and (not (options.branch or options.tag))
+        puts("WARNING: No ref was given! Please specify either --tag or --branch.")
+        return false
+      end # if
+
+      if options.tag and options.branch
+        puts("Please specify either --tag or --branch (but not both).")
+        return false
+      end # if
+    end # self.good_args?
+  end # class Git2JSS
+end # module Git2JSS
